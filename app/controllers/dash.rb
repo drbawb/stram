0a30a0697a46c2da7da1d4997e7027f3345a93e7
dr(bawb) <drbawb@fatalsyntax.com>
2017-08-31 13:56:07 -0500

    (init) valestream v0.1
    Provides a minimally functional HLS player & IRC chat client.

---
+++ app/controllers/dash.rb
@@ -0,0 +1,31 @@
+Stram::App.controllers :dash do
+ 
+  get '/', :map => '/' do
+    render "dash/index"
+  end
+
+  get '/login', :map => '/login' do
+    halt 500
+  end
+
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+
+end
---
+++ app/helpers/dash_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Stram
+  class App
+    module DashHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers DashHelper
+  end
+end
