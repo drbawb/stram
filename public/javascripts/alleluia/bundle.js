3ec51c1b3db6f3cbcdc4d062a79693897fcaab9f
dr(bawb) <drbawb@fatalsyntax.com>
2017-10-07 10:29:13 -0500

    add emotes & get rid of input submit btn

--- public/javascripts/alleluia/bundle.js
+++ public/javascripts/alleluia/bundle.js
@@ -311,20 +311,44 @@ console.log("starting text-mode client");
 $(document).ready(function() {
 	var client  = require('./chat/events.js'); // TODO: really, really bad module name.
 
-  var funnyBusiness = ["47735570", "81500175"];
+  var funnyBusiness = ["81500175"];
   var uidToTwitchId = {};
 
   var ui = {
+    // main container
+    chatBox:  $(".chat-box"),
+    movieBox: $(".movie-box"),
+    
+    // chat input
     messages:  $("#alleluia-messages"),
     inputBox:  $("#alleluia-input"),
-    submitBtn: $("#alleluia-submit"),
+    emoteBtn:  $("#alleluia-emotes"),
+    emoteBox:  $("#alleluia-emote-menu")
   };
 
   var emotes = {
-    ":valeHype:":   {path: "/images/valemotes/hype2changes56.png"},
-    ":valeLurk:": {path: "/images/valemotes/valeLurk56.png"},
-    ":valeDabL:": {path: "/images/valemotes/dab 56.png"},
-    ":valeDabR:": {path: "/images/valemotes/valeDabR56.png"}
+    ":valeBattle:":             {path: "/images/valemotes/valeBattle.png"},
+    ":valeBlush:":              {path: "/images/valemotes/valeBlush.png"},
+    ":valeCry:":                {path: "/images/valemotes/valeCry.png"},
+    ":valeDabL:":               {path: "/images/valemotes/valeDabL.png"},
+    ":valeDabR:":               {path: "/images/valemotes/valeDabR.png"},
+    ":valeEdgy:":               {path: "/images/valemotes/valeEdgy.png"},
+    ":valeFail:":               {path: "/images/valemotes/valeFail.png"},
+    ":valeGasm:":               {path: "/images/valemotes/valeGasm.png"},
+    ":valeGG:":                 {path: "/images/valemotes/valeGG.png"},
+    ":valeGrrr:":               {path: "/images/valemotes/valeGrrr.png"},
+    ":valeHug:":                {path: "/images/valemotes/valeHug.png"},
+    ":valeHype:":               {path: "/images/valemotes/valeHype.png"},
+    ":valeLove:":               {path: "/images/valemotes/valeLove.png"},
+    ":valeLurk:":               {path: "/images/valemotes/valeLurk.png"},
+    ":valeNano:":               {path: "/images/valemotes/valeNano.png"},
+    ":valeowValeHealsGoodMan:": {path: "/images/valemotes/valeowValeHealsGoodMan.png"},
+    ":valeRIP:":                {path: "/images/valemotes/valeRIP.png"},
+    ":valeShrug:":              {path: "/images/valemotes/valeShrug.png"},
+    ":valeSmug:":               {path: "/images/valemotes/valeSmug.png"},
+    ":valeThink:":              {path: "/images/valemotes/valeThink.png"},
+    ":valeWave:":               {path: "/images/valemotes/valeWave.png"},
+    ":valeYo:":                 {path: "/images/valemotes/valeYo.png"}
   };
 
   var scanForEmotes = function(msg) {
@@ -396,12 +420,53 @@ $(document).ready(function() {
     }
   });
 
-  ui.submitBtn.on("click", function() {
-    // grab the msg buffer
-    var msg = ui.inputBox.val(); ui.inputBox.val("");
-    console.log("sending: " + msg);
-    client.sendMessage(msg);
+  // submit message when enter is pressed
+  ui.inputBox.on("keyup", function(evt) {
+    if (evt.keyCode === 13) { // press enter
+      var msg = ui.inputBox.val(); ui.inputBox.val("");
+      console.log("sending: " + msg);
+      client.sendMessage(msg);     
+    };
   });
+
+  // display emote menu
+  ui.emoteBtn.on("click", function(evt) {
+    ui.emoteBox.removeClass("hidden");
+    evt.stopPropagation();
+  });
+
+  // hide emote menu if any negative space in chat is clicked
+  ui.chatBox.on("click", function() {
+    ui.emoteBox.addClass("hidden");
+  });
+
+  // hide emote menu on pc if escape is pressed
+  ui.chatBox.on("keyup", function(evt) {
+    if (evt.keyCode === 27) { // press escape
+      ui.emoteBox.addClass("hidden");
+    }
+  });
+
+  // populate the emote menu
+  for (var key in emotes) {
+    if (!emotes.hasOwnProperty(key)) { continue; }
+    let shortCode = key;
+    let imgUrl    = emotes[key].path;
+
+    // set up a clickable icon
+    let emoteEl   = $("<div>");
+    let emoteIcon = $("<img>");
+    emoteIcon.attr("src", imgUrl);
+    emoteIcon.addClass("emote");
+    emoteIcon.appendTo(emoteEl);
+    emoteIcon.on("click", function(evt) {
+      console.log("clicked on: " + shortCode);
+      ui.inputBox.val(`${ui.inputBox.val()} ${shortCode}`);
+      evt.stopPropagation();
+    });
+
+    emoteEl.appendTo(ui.emoteBox);
+  }
 });
 
 
