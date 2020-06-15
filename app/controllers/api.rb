fe397df16a2ddde01a8866f7965f1f604289573b
dr(bawb) <drbawb@fatalsyntax.com>
2020-06-15 15:46:02 -0500

    clean up api key authorization logic

--- app/controllers/api.rb
+++ app/controllers/api.rb
@@ -2,11 +2,7 @@ Stram::App.controllers :api do
 
   post :twitch_token, with: :user_id, provides: :json, csrf_protection: false do
     # verify the authorization matches the bot api key
-    auth_header = request.env["HTTP_AUTHORIZATION"]
-    
-    if not auth_header&.eql? ENV["BOTMAIN_API_KEY"]
-      logger.info "header: #{auth_header}"
-      logger.info "api key: #{ENV["BOTMAIN_API_KEY"]}"
+    unless api_check_key(request.env["HTTP_AUTHORIZATION"])
       response = {success: false, message: "Unauthorized API key."}
       halt 403, response.to_json
     end
@@ -16,14 +12,15 @@ Stram::App.controllers :api do
       "Client-ID": ENV["TWITCH_CLIENT_ID"],
       "Accept" => "application/vnd.twitchtv.v5+json"
     }
-    
+   
+    logger.debug "fetching twitch profile for user #{params[:user_id]}" 
     response  = HTTP.headers(oauth_opts).get("https://api.twitch.tv/kraken/users/#{params[:user_id]}")
     user      = JSON.parse(response)
     user_name = user["name"]
     user_id   = user["_id"]
  
     # create a new token for this twitch profile 
-    logger.info "tt :: #{user_name} @ #{user_id}"
+    logger.debug "creating twitch token for #{user_name} (#{user_id})"
     tt = TwitchToken.new(username: user_name, twitch_id: user_id)
     if tt.save
       {success: true, token_id: tt.id}.to_json
---
+++ app/helpers/api_helper.rb
@@ -0,0 +1,19 @@
+module Stram
+  class App
+    module ApiHelper
+      def api_check_key(key)
+        if ENV["BOTMAIN_API_KEY"].nil? or ENV["BOTMAIN_API_KEY"].empty?
+          logger.warn "API called, but API key is not set."
+          return false
+        end
+
+        logger.info "key: #{key}"
+        logger.info "env: #{ENV["BOTMAIN_API_KEY"]}"
+        return ENV["BOTMAIN_API_KEY"].eql? key
+      end
+    end
+
+    helpers ApiHelper
+  end
+end
+
